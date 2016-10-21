98f9ee8fc815d054a0b0c59305d385c135b7fb81
Rich Churcher <rich.churcher@gmail.com>
2016-10-21 18:31:40 +1300

    Flesh out

--- components/grid.jsx
+++ components/grid.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {range} from 'lodash'
+
+import Cell from './cell'
 
 export default React.createClass({
+  propTypes: {
+    width: React.PropTypes.number,
+    height: React.PropTypes.number
+  },
+
   render () {
+    const cells = range(this.props.width * this.props.height)
+      .map((n) => {
+        return <Cell key={n} />
+      })
+
     return (
-      <main></main>
+      <main>{cells}</main>
     )
   }
 })
